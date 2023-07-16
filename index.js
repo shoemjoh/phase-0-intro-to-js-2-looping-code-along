8383f9487b4302e49dac5dc979f2e779d93c1fe2
John <shoemjoh@gmail.com>
2023-07-16 15:51:08 -0400

    Looping lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards(attendees, event) {
+    const thankyous = [];
+    for (let i = 0; i < attendees.length; i++) {
+        thankyous.push(`Thank you, ${attendees[i]}, for the wonderful surprise gift!`)
+    }
+    return thankyous;
+}
+
+
+
+
+
+function countDown(i) {
+    let countNum = i;
+    while (countNum >= 0) {
+        console.log(countNum)
+        countNum--;
+    }
+}
+
+countDown(17);
